d3299fbbc127a0f648832c1aa30a28c97532776b
emmadesuyrot <desue472@newschool.edu>
2025-02-07 19:16:44 -0500

    JavaScript and ID added (class demo vid: 2:1716)

---
+++ assets/arena.js
@@ -0,0 +1,29 @@
+// This allows us to process/render the descriptions, which are in Markdown!
+// More about Markdown: https://en.wikipedia.org/wiki/Markdown
+let markdownIt = document.createElement('script')
+markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
+document.head.appendChild(markdownIt)
+
+let channelSlug = 'what-it-feels-like-to-fall-in-love' // The “slug” is just the end of the URL
+
+let placeChannelInfo = (data) => {
+	// Target some elements in your HTML:
+	let channelTitle = document.querySelector('#channel-title')
+	let channelDescription = document.querySelector('#channel-description')
+	let channelCount = document.querySelector('#channel-count')
+	let channelLink = document.querySelector('#channel-link')
+
+	// Then set their content/attributes to our data:
+	channelTitle.innerHTML = data.title
+	channelDescription.innerHTML = window.markdownit().render(data.metadata.description) // Converts Markdown → HTML
+	channelCount.innerHTML = data.length
+	channelLink.href = `https://www.are.na/channel/${channelSlug}`
+}
+
+// Now that we have said what we can do, go get the data:
+fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
+	.then((response) => response.json()) // Return it as JSON data
+	.then((data) => { // Do stuff with the data
+		console.log(data) // Always good to check your response!
+		placeChannelInfo(data)
+	})
