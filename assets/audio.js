f9002df00803978e9cbfbc3300fa0c0b47cfdb17
emmadesuyrot <desue472@newschool.edu>
2025-02-23 19:48:52 -0500

    Added pause and play icon and responsive styling

--- assets/audio.js
+++ assets/audio.js
@@ -2,21 +2,25 @@
 	// Added an additional script file to add the music Play and Pause button
 	// the script matches the html <audio> inside the <div class="navbar">
 
+	// play & pause icon: https://stackoverflow.com/questions/22885702/html-for-the-pause-symbol-in-audio-and-video-control
+
 document.addEventListener("DOMContentLoaded", function() {
 	const audio = new Audio("assets/audio/GibranAlcocer-Idea10.mp3");
+		audio.loop = true;
 	const buttons = document.querySelectorAll("button");
 
 	buttons.forEach(button => {
 		// forEach (loops over each button)
+		button.innerHTML = "⏵︎";
 	
 		button.addEventListener("click", () => {
 		// If the audio is playing, pause it, else play it
 		if (audio.paused) {
 			audio.play();
-			button.textContent = "Pause Music";
+			button.textContent = "⏸︎";
 		} else {
 			audio.pause();
-			button.textContent = "Play Music";
+			button.textContent = "⏵︎";
 		}
 		});
 	});
